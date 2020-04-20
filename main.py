6a68ae611bd6b0135b884c78ef8d7eb6fba7e5a7
MrXELy <misterxely@gmail.com>
2020-04-20 15:39:12 +0200

    Change main behaviour to fetch lyrics when song changes
    Use hasTitleChanged to search for lyrics when the song playing changes, instead of every seconds.
    Close file

--- main.py
+++ main.py
@@ -3,28 +3,35 @@ import songinfo as si
 from time import sleep
 
 windowID = si.initSpotify()
-windowTitle = si.getWindowTitle(windowID)
+oldWindowTitle = ""
+newWindowTitle = si.getWindowTitle(windowID)
 
-while 1:
-    windowTitle = si.getWindowTitle(windowID)
-    if windowTitle != -1:
-        artist, song = si.getSongInfo(windowTitle)
+while True:
+    newWindowTitle = si.getWindowTitle(windowID)
+    if si.hasTitleChanged(newWindowTitle, oldWindowTitle):
 
-        URL = si.getSearchURL(artist + " " + song)
+        if si.isSongPlaying(newWindowTitle):
+            artist, song = si.getSongInfo(newWindowTitle)
+            print(artist + " - " + song)
 
-        lyricsURL = si.getLyricsURL(si.getSoup(URL))
+            URL = si.getSearchURL(artist + " " + song)
 
-        if lyricsURL == -1:
-            print("No lyrics ? " + URL)
-            lyrics = "Can't find the lyrics... "
-        else:    
-            lyrics = artist + " - " + song + " " + lyricsURL + " " + si.getLyrics(si.getSoup(lyricsURL))
-            
-        f = open("lyrics.txt", "w+")
-        f.write(lyrics)
+            lyricsURL = si.getLyricsURL(si.getSoup(URL))
+
+            if lyricsURL == -1:
+                print("No lyrics ? " + URL)
+                lyrics = "Can't find the lyrics... " + URL
+            else:    
+                lyrics = artist + " - " + song + " " + lyricsURL + " " + si.getLyrics(si.getSoup(lyricsURL))
+                
+            f = open("lyrics.txt", "w+")
+            f.write(lyrics)
+            f.close()
+        else:
+            print("No song playing")
+
+        oldWindowTitle = newWindowTitle
+        
+
 
     sleep(1)
-
-
-
- 
